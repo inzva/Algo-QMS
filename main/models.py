81145cb68ae577f947fb89abefec254b84725efe
ZE0TRON <cmpbilge@gmail.com>
2017-11-12 15:44:58 +0300

    Question Folder creation when question added implemented

--- main/models.py
+++ main/models.py
@@ -1,8 +1,12 @@
 from django.conf import settings
 from django.db import models
 
+import os
+from django.db.models.signals import post_save
+
 
 class Problem(models.Model):
+    question_name = models.TextField()
     statement = models.TextField()
     memory_limit = models.PositiveIntegerField()
     time_limit = models.PositiveIntegerField()
@@ -34,6 +38,20 @@ class SubmissionTestCaseResult(models.Model):
     submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
     test_case = models.ForeignKey(TestCase, on_delete=models.CASCADE)
     succeeded = models.BooleanField()
+
+
+def problem_saved(instance, *args, **kwargs):
+    directory = "Problems" + os.sep
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        os.chdir(directory)
+    else:
+        os.chdir(directory)
+    os.makedirs(instance.question_name, exist_ok=True)
+    os.chdir(".." + os.sep)
+
+
+post_save.connect(problem_saved, sender=Problem)
 
 
 class Question(models.Model):
