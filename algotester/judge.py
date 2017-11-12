8f884b0918184100882081a9722e4101ec05975e
Ze0tron <ze0tron@Ze0tron-MacBook-Pro.local>
2017-11-12 11:20:20 +0300

    Random judger signature implemented

---
+++ algotester/judge.py
@@ -0,0 +1,30 @@
+import random
+random.seed()
+def judge(file_path,question,numberOfTestCases,scores):
+    runtime=[]
+    timelimit=[]
+    correct=[]
+    wrong=[]
+    for i in range(numberOfTestCases):
+        b = random.randint(0,4)
+        if(b==0):
+            runtime.append(0)
+            timelimit.append(0)
+            correct.append(1)
+            wrong.append(0)
+        elif(b==1):
+            runtime.append(0)
+            timelimit.append(0)
+            correct.append(0)
+            wrong.append(1)
+        elif(b==2):
+            runtime.append(1)
+            timelimit.append(0)
+            correct.append(0)
+            wrong.append(0)
+        elif(b==3):
+            runtime.append(0)
+            timelimit.append(1)
+            correct.append(0)
+            wrong.append(0)
+    return runtime,timelimit,correct,wrong
