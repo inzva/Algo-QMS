6096cd2df94ec73d0da4350b9bccc755a6517763
Ze0tron <ze0tron@Ze0tron-MacBook-Pro.local>
2017-11-12 12:19:03 +0300

    Judge py signature changed running times added

--- algotester/judge.py
+++ algotester/judge.py
@@ -1,32 +1,34 @@
 import random
 random.seed()
-def judge(file_path,question,numberOfTestCases,scores):
-    runtime=[]
-    timelimit=[]
-    correct=[]
-    wrong=[]
-    score=0
+
+
+def judge(file_path, question, numberOfTestCases, scores):
+    runtime = []
+    timelimit = []
+    correct = []
+    wrong = []
+    score = 0
     for i in range(numberOfTestCases):
-        b = random.randint(0,4)
-        if(b==0):
+        b = random.randint(0, 4)
+        if(b == 0):
             runtime.append(0)
             timelimit.append(0)
             correct.append(1)
             wrong.append(0)
-        elif(b==1):
+        elif(b == 1):
             runtime.append(0)
             timelimit.append(0)
             correct.append(0)
             wrong.append(1)
-            score+=scores[i]
-        elif(b==2):
+            score += scores[i]
+        elif(b == 2):
             runtime.append(1)
             timelimit.append(0)
             correct.append(0)
             wrong.append(0)
-        elif(b==3):
+        elif(b == 3):
             runtime.append(0)
             timelimit.append(1)
             correct.append(0)
             wrong.append(0)
-    return runtime,timelimit,correct,wrong,score
+    return runTimeErrors, timeLimitErrors, correctAnswers, wrongAnswers, runningTimes, score
